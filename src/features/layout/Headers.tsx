35cc7faa7baa225b9991434ac3d59a04f72d51f5
Maxime <maxime.celeste1221@gmail.com>
2024-06-18 11:55:57 +0400

    add logo icon title

--- src/features/layout/Headers.tsx
+++ src/features/layout/Headers.tsx
@@ -3,7 +3,9 @@ import { LoggedInButton } from "../auth/LoggedInButton";
 export const Headers = async () => {
   return (
     <div className="flex items-center gap-4">
-      <h1 className="font-bold text-lg">trusty-reviews.com</h1>
+      <h1 className="font-bold text-lg">
+        {/* <Image src="/icon-title.png" width={64} alt="icon page" /> */}
+      </h1>
       <LoggedInButton />
     </div>
   );

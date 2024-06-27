4333cb30aad089cc4624f859fea3eaed6bc2b089
Maxime <maxime.celeste1221@gmail.com>
2024-06-27 09:45:53 +0400

    middleware fix error

--- app/(customer)/products/[productId]/edit/product.action.ts
+++ app/(customer)/products/[productId]/edit/product.action.ts
@@ -1,11 +1,21 @@
 "use server";
 
+import { prisma } from "@/prisma";
 import { userAction } from "@/safe-action";
 import { ProductSchema } from "./product.schema";
 
 export const createProductAction = userAction(
   ProductSchema,
-  async (input, context) => {}
+  async (input: any, context: { user: { id: any } }) => {
+    const product = await prisma.product.create({
+      data: {
+        ...input,
+        userId: context.user.id,
+      },
+    });
+
+    return product;
+  }
 );
 
 export const editProductAction = async () => {};
